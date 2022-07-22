b1fa7170fa5860d65f4e23d81af770527af24634
Pusikovsa <s.puzikov59@gmail.com>
2022-07-22 17:02:36 +0300

    created AsyncTaskExample activity

---
+++ app/src/main/java/ru/synergy/asyntaskexample/AsyncTaskExample.java
@@ -0,0 +1,14 @@
+package ru.synergy.asyntaskexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AsyncTaskExample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asynk_task_example);
+    }
+}
